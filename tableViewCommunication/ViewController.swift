65e08a46ab594a9c893fdee1b824abad0ee78120
Jeisson Gonzalez <jsn8802@gmail.com>
2018-02-22 23:24:16 -0500

    Initial Commit

---
+++ tableViewCommunication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableViewCommunication
+//
+//  Created by Jeisson Gonzalez on 22/02/18.
+//  Copyright © 2018 Jeisson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
